1ad304e85f73f2e48d993c9b4239bf5b01ae68a2
Noam <noam@fireflymigration.com>
2019-09-19 16:52:55 +0300

    עובד עברית

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,11 +11,11 @@ import { Roles, AdminGuard } from './users/roles';
 
 
 const routes: Routes = [
-  { path: 'Home', component: HomeComponent },
-  { path: 'User Accounts', component: UsersComponent, canActivate: [AdminGuard] },
+  { path: 'Home', component: HomeComponent,data: { name: "בית" }  },
+  { path: 'User Accounts', component: UsersComponent, canActivate: [AdminGuard], data: { name: "משתמשים" } },
 
-  { path: 'Register', component: RegisterComponent, canActivate: [NotSignedInGuard] },
-  { path: 'Account Info', component: UpdateInfoComponent, canActivate: [SignedInGuard] },
+  { path: 'Register', component: RegisterComponent, canActivate: [NotSignedInGuard] , data: { name: "הרשם" } },
+  { path: 'Account Info', component: UpdateInfoComponent, canActivate: [SignedInGuard], data: { name: "עדכון פרטים" }  },
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: '**', redirectTo: '/Home', pathMatch: 'full' }
 
